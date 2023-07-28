33bb09ce54f57860e7d6dc4b09b95a0561658ae7
Blake Collins <blakeac369@gmail.com>
2023-07-28 12:31:58 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import Rightbar from "./components/Rightbar"
 import Navbar from "./components/Navbar"
 import Add from "./components/Add"
 import { Box, Stack } from "@mui/material";
+import { useState } from "react"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 
 
 function App() {
+  const [mode, setMode] = useState("dark")
+
+  const darkTheme = createTheme ({
+    palette:{
+      mode: mode,
+    }
+  })
   return (
-    <Box>
+    <ThemeProvider theme={darkTheme}>
+    <Box bgcolor={"background.default"} color={"text.primary"}>
     <Navbar />
     <Stack direction="row" spacing={2} justifyContent="space-between">
-      <Sidebar />
+      <Sidebar setMode={setMode} mode={mode}/>
       <Feed />
       <Rightbar />
     </Stack>
       <Add />
     </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { AccountBox, Article, Group, Home, ModeNight, Person, Settings, Storefro
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({ mode, setMode}) => {
   return (
     <Box 
     flex={1} p={2} 
@@ -71,7 +71,7 @@ const Sidebar = () => {
               <ListItemIcon>
                 <ModeNight />
               </ListItemIcon>
-              <Switch color="warning"/>
+              <Switch color="primary" onChange={(e) =>setMode(mode === 'light' ? "dark" : "light")}/>
             </ListItemButton>
           </ListItem>
         </List>
